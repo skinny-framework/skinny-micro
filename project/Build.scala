e8347dcd3ae1eee82e429a17e42ba7b0f1c3475c
Kazuhiro Sera <seratch@gmail.com>
2015-09-10 01:05:10 +0900

    version 0.9.9

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import scala.language.postfixOps
 
 object SkinnyMicroBuild extends Build {
 
-  lazy val currentVersion = "0.9.8"
+  lazy val currentVersion = "0.9.9"
 
   lazy val json4SVersion = "3.3.0.RC4"
   lazy val scalatraTestVersion = "2.3.1"
