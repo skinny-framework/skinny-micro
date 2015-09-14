ff2c7be61d5c8d4dda13efe4ca40e02bb6ee16fe
Kazuhiro Sera <seratch@gmail.com>
2015-09-15 02:28:45 +0900

    Add more concurrency tests

--- micro/src/test/scala/example/ConcurrencyTestSpec.scala
+++ micro/src/test/scala/example/ConcurrencyTestSpec.scala
@@ -5,9 +5,10 @@ import skinny.micro._
 
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class ConcurrencyTestSpec extends ScalatraFlatSpec {
+
+  implicit val context = SkinnyMicroBase.defaultExecutionContext
 
   addFilter(new AsyncWebApp {
     get("/async") { implicit ctx =>
@@ -17,6 +18,31 @@ class ConcurrencyTestSpec extends ScalatraFlatSpec {
     }
     get("/sync") { implicit ctx =>
       "OK"
+    }
+    get("/content-type-1") { implicit ctx =>
+      Thread.sleep(50)
+      Future {
+        Thread.sleep(50)
+        contentType = "text/csv"
+        Thread.sleep(20)
+        "foo,bar,baz"
+      }
+    }
+    get("/content-type-2") { implicit ctx =>
+      Thread.sleep(50)
+      contentType = "text/csv"
+      Future {
+        Thread.sleep(20)
+        "foo,bar,baz"
+      }
+    }
+
+    get("/content-type-3") { implicit ctx =>
+      Thread.sleep(50)
+      Future {
+        Thread.sleep(50)
+        Ok(body = "foo,bar,baz", contentType = Some("text/csv"))
+      }
     }
   }, "/*")
 
@@ -47,6 +73,48 @@ class ConcurrencyTestSpec extends ScalatraFlatSpec {
   it should "work fine in async mode for sequential requests 2" in {
     (1 to 10).foreach { _ =>
       get("/async") { body should equal("OK") }
+    }
+  }
+
+  it should "set content-type in async mode 1" in {
+    (1 to 5).foreach { _ =>
+      Await.result(Future.sequence((1 to 10).map { _ =>
+        Future {
+          get("/content-type-1") {
+            status should equal(200)
+            header("Content-Type") should equal("text/csv; charset=UTF-8")
+            body should equal("foo,bar,baz")
+          }
+        }
+      }), 3.seconds)
+    }
+  }
+
+  it should "set content-type in async mode 2" in {
+    (1 to 5).foreach { _ =>
+      Await.result(Future.sequence((1 to 10).map { _ =>
+        Future {
+          get("/content-type-2") {
+            status should equal(200)
+            header("Content-Type") should equal("text/csv; charset=UTF-8")
+            body should equal("foo,bar,baz")
+          }
+        }
+      }), 3.seconds)
+    }
+  }
+
+  it should "set content-type in async mode 3" in {
+    (1 to 5).foreach { _ =>
+      Await.result(Future.sequence((1 to 10).map { _ =>
+        Future {
+          get("/content-type-3") {
+            status should equal(200)
+            header("Content-Type") should equal("text/csv; charset=UTF-8")
+            body should equal("foo,bar,baz")
+          }
+        }
+      }), 3.seconds)
     }
   }
 
